9abc78b76f9e28846ea148e969305f74f610281b
SC-WSKun <540610423@qq.com>
2024-12-16 13:48:17 +0800

    feat: tailwind prettier

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,11 @@
-import Link from "next/link"
-export default function Header(){
+import Link from "next/link";
+export default function Header() {
     return (
-        <div className="w-full flex flex-row justify-between">
-            <Link href={'/home'}>Home</Link>
-            <Link href={'/blog'}>Blog</Link>
-            <Link href={'/photo'}>Photo</Link>
-            <Link href={'/project'}>Project</Link>
+        <div className="flex h-28 w-full flex-row items-center justify-between bg-beige">
+            <Link href={"/home"}>Home</Link>
+            <Link href={"/blog"}>Blog</Link>
+            <Link href={"/photo"}>Photo</Link>
+            <Link href={"/project"}>Project</Link>
         </div>
-    )
+    );
 }
