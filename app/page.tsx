29be8ab8bbf0a0d332a768d35315b284b5feeef7
kunkun <540610423@qq.com>
2025-01-25 21:35:02 +0800

    feat: home background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,30 @@
+"use client";
 
-"use client"
+import { BlogError } from "@/components/BlogError";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      <Clock />
-    </div>
-  )
+	return (
+		<div className="grow bg-[url(../public/image/home-bg.png)] bg-cover bg-center">
+			<Clock />
+		</div>
+	);
 }
 
 function Clock() {
-  return <canvas id="clock" width="500" height="500"></canvas>
+	const clock = useRef<HTMLCanvasElement>(null);
+
+	const renderClock = () => {
+		const canvas = clock.current;
+		if (!canvas) {
+			throw new BlogError("canvas not found");
+		}
+		canvas.width = 600;
+		canvas.height = 300;
+	};
+	useEffect(() => {
+		renderClock();
+	}, []);
+
+	return <canvas ref={clock} className=""></canvas>;
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,32 +1,42 @@
-"use client"
+"use client";
 import Link from "next/link";
-import { motion } from "motion/react"
+import { motion } from "motion/react";
 import { aestera } from "@/app/fonts";
 import { ReactNode } from "react";
 import Contact from "./Contact";
 export default function Header() {
-  return (
-    <div className="flex h-28 w-full flex-row items-center justify-around gap-40 bg-beige px-20 font-mono text-xl border-b-2 border-tan">
-      <p className={aestera.className + " text-2xl"}>Kunn Blog</p>
-      <div className="grid grid-cols-4 h-full grow">
-        <NavLink href={"/"} >Home</NavLink>
-        <NavLink href={"/blog"}>Blog</NavLink>
-        <NavLink href={"/photo"} >Photo</NavLink>
-        <NavLink href={"/project"} >Project</NavLink>
-      </div>
-      <Contact /> 
-    </div>
-  );
+	return (
+		<div className="flex w-full flex-row items-center justify-around gap-40 border-b-2 border-tan bg-beige px-20 py-4 font-mono text-xl">
+			<p className={aestera.className + " text-2xl"}>Kunn Blog</p>
+			<div className="grid h-full grow grid-cols-4">
+				<NavLink href={"/"}>Home</NavLink>
+				<NavLink href={"/blog"}>Blog</NavLink>
+				<NavLink href={"/photo"}>Photo</NavLink>
+				<NavLink href={"/project"}>Project</NavLink>
+			</div>
+			<Contact />
+		</div>
+	);
 }
 
-function NavLink(props: { href: string, children: ReactNode }) {
-  const { href, children } = props;
-  return (
-    <Link className="flex h-full items-center justify-center grow tracking-wide" href={href}>
-      <motion.div
-        className="w-4/5 h-4/5 flex items-center justify-center rounded-3xl"
-        whileHover={{ scale: 1.1, backgroundColor: 'var(--tan)', color: 'white' }}
-        whileTap={{ scale: 0.95 }}
-      >{children}</motion.div></Link>
-  )
+function NavLink(props: { href: string; children: ReactNode }) {
+	const { href, children } = props;
+	return (
+		<Link
+			className="flex h-20 grow items-center justify-center tracking-wide"
+			href={href}
+		>
+			<motion.div
+				className="flex h-4/5 w-4/5 items-center justify-center rounded-3xl"
+				whileHover={{
+					scale: 1.1,
+					backgroundColor: "var(--tan)",
+					color: "white",
+				}}
+				whileTap={{ scale: 0.95 }}
+			>
+				{children}
+			</motion.div>
+		</Link>
+	);
 }
